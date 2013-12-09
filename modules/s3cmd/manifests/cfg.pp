aa9c9914a135a417f66d4446b1f5684b1f6290ea
Ben Chavet <ben.chavet@lullabot.com>
2013-12-09 16:42:26 -0600

    import s3cmd module

---
+++ modules/s3cmd/manifests/cfg.pp
@@ -0,0 +1,18 @@
+define s3cmd::cfg (
+    $user = $title,
+    $group = $title,
+    $home = "/home/$title",
+    $access_key,
+    $gpg_passphrase,
+    $secret_key,
+    $use_https = 'False',
+) {
+
+    file { "$home/.s3cfg":
+        content => template('s3cmd/s3cfg.erb'),
+        owner   => $user,
+        group   => $group,
+        mode    => '0600',
+    }
+
+}
---
+++ modules/s3cmd/manifests/init.pp
@@ -0,0 +1,3 @@
+class s3cmd {
+    if !defined(Package['s3cmd']) { package { 's3cmd': } }
+}

11e9700dc6aeb2a6c92885749f6ff1a9718b4183
Ben Chavet <ben.chavet@lullabot.com>
2013-12-11 12:26:10 -0600

    add memcached module

---
+++ modules/memcached/manifests/init.pp
@@ -0,0 +1,24 @@
+class memcached (
+    $memory = 64,
+    $port = 11211,
+    $user = 'memcache',
+    $listen = '127.0.0.1',
+    $connections = 1024,
+) {
+    if !defined(Package['memcached']) { package { 'memcached': } }
+
+    service { 'memcached':
+        ensure  => running,
+        enable  => true,
+        require => Package['memcached'],
+    }
+
+    file { '/etc/memcached.conf':
+        content => template('memcached/etc/memcached.conf.erb'),
+        owner   => 'root',
+        group   => 'root',
+        mode    => '0644',
+        require => Package['memcached'],
+        notify  => Service['memcached'],
+    }
+}

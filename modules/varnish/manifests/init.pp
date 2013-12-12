b34de260fc6b14a8b125580a65113b7a2d12e92b
Ben Chavet <ben.chavet@lullabot.com>
2013-12-12 12:50:25 -0600

    add varnish module

---
+++ modules/varnish/manifests/init.pp
@@ -0,0 +1,42 @@
+class varnish (
+    $address     = ':80',
+    $management  = 'localhost:6082',
+    $storage     = 'malloc,256m',
+    $user        = 'varnish',
+    $group       = 'varnish',
+    $nfiles      = 131072,
+    $memlock     = 82000,
+    $varnishncsa = true,
+    $varnishlog  = false,
+) {
+
+    File {
+        require => Package['varnish'],
+    }
+
+    Service {
+        ensure => running,
+        enable => true,
+    }
+
+    if !defined(Package['varnsh']) { package { 'varnish': } }
+
+    file { '/etc/default/varnish':
+        content => template('varnish/etc/default/varnish.erb'),
+        notify  => Service['varnish'],
+    }
+
+    file { '/etc/default/varnishlog':
+        content => template('varnish/etc/default/varnishlog.erb'),
+        notify  => Service['varnishlog'],
+    }
+
+    file { '/etc/default/varnishncsa':
+        content => template('varnish/etc/default/varnishncsa.erb'),
+        notify  => Service['varnishncsa'],
+    }
+
+    service { 'varnish': }
+    service { 'varnishlog': }
+    service { 'varnishncsa': }
+}

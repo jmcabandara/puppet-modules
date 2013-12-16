5a0bd0036c7be7ddfe699487a8490ec3e52b16f1
Ben Chavet <ben.chavet@lullabot.com>
2013-12-16 14:39:20 -0600

    drush module TODO

--- modules/drush/manifests/init.pp
+++ modules/drush/manifests/init.pp
@@ -18,4 +18,15 @@ class drush (
         require => File["${installdir}/drush"],
     }
 
+    # TODO: install Console_Table-1.1.3
+    ##    Drush needs to download a library from                               [error]
+    ##    http://download.pear.php.net/package/Console_Table-1.1.3.tgz in order
+    ##    to function, and the attempt to download this file automatically
+    ##    failed because you do not have permission to write to the library
+    ##    directory /usr/local/share/drush-6.2.0/lib. To continue you will need
+    ##    to manually download the package from
+    ##    http://download.pear.php.net/package/Console_Table-1.1.3.tgz, extract
+    ##    it, and copy the directory into your /usr/local/share/drush-6.2.0/lib
+    ##    directory.
+
 }

923d3faad909bfc39e0a7360bf4c3157228c3826
Ben Chavet <ben.chavet@lullabot.com>
2014-01-06 12:31:15 -0600

    add a netatalk module

---
+++ modules/netatalk/manifests/init.pp
@@ -0,0 +1,33 @@
+class netatalk (
+    $protocol = '-tcp -noddp',
+    $uamlist = 'uams_dhx.so,uams_dhx2.so',
+    $password = '-nosavepassword',
+    $guestname = 'nobody',
+    $mimicmodel = 'Xserve',
+    $volumes = { '~/' => 'Home Directory' },
+) {
+
+    File {
+        require => Package['netatalk'],
+        notify  => Service['netatalk'],
+    }
+
+    if !defined(Package['netatalk']) { package { 'netatalk': } }
+    if !defined(Package['avahi-daemon']) { package { 'avahi-daemon': } }
+
+    file { '/etc/netatalk/afpd.conf':
+        content => template('netatalk/etc/netatalk/afpd.conf.erb'),
+    }
+
+    file { '/etc/netatalk/AppleVolumes.default':
+        content => template('netatalk/etc/netatalk/AppleVolumes.default.erb'),
+    }
+
+    service { 'netatalk':
+        ensure    => running,
+        enable    => true,
+        hasstatus => false,
+        status    => 'ps -h --pid `cat /var/run/afpd.pid`',
+        require   => Package['netatalk'],
+    }
+}

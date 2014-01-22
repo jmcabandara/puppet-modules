d5c6efa0a27af946f1d850bba7d9f84650cdfefe
Ben Chavet <ben.chavet@lullabot.com>
2014-01-22 17:44:38 -0600

    update php5-xsl to trusty

--- modules/php/manifests/xsl.pp
+++ modules/php/manifests/xsl.pp
@@ -1,8 +1,8 @@
 class php::xsl {
     if !defined(Package['php5-xsl']) { package { 'php5-xsl': require => Package['php5-cli'] } }
 
-    file { '/etc/php5/conf.d/xsl.ini':
-        content => template('php/etc/php5/conf.d/xsl.ini.erb'),
+    file { '/etc/php5/mods-available/xsl.ini':
+        content => template('php/etc/php5/mods-available/xsl.ini.erb'),
         require => Package['php5-xsl'],
         notify  => Exec['php::restart'],
     }

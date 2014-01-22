83ed6b041e72fddf70c7a6b325c8d2d8c2a23176
Ben Chavet <ben.chavet@lullabot.com>
2014-01-22 17:25:06 -0600

    php module configs are in mods-available now

--- modules/php/manifests/curl.pp
+++ modules/php/manifests/curl.pp
@@ -3,8 +3,8 @@ class php::curl (
 ) {
     if !defined(Package['php5-curl']) { package { 'php5-curl': require => Package['php5-cli'] } }
 
-    file { '/etc/php5/conf.d/curl.ini':
-        content => template('php/etc/php5/conf.d/curl.ini.erb'),
+    file { '/etc/php5/mods-available/curl.ini':
+        content => template('php/etc/php5/mods-available/curl.ini.erb'),
         require => Package['php5-curl'],
         notify  => Exec['php::restart'],
     }

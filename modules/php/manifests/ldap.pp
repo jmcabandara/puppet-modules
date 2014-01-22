e4cef3f0420b80c8890b68b198e2bc0a864519b2
Ben Chavet <ben.chavet@lullabot.com>
2014-01-22 17:33:21 -0600

    update php5-ldap to trusty

--- modules/php/manifests/ldap.pp
+++ modules/php/manifests/ldap.pp
@@ -3,8 +3,8 @@ class php::ldap (
 ) {
     if !defined(Package['php5-ldap']) { package { 'php5-ldap': require => Package['php5-cli'] } }
 
-    file { '/etc/php5/conf.d/ldap.ini':
-        content => template('php/etc/php5/conf.d/ldap.ini.erb'),
+    file { '/etc/php5/mods-available/ldap.ini':
+        content => template('php/etc/php5/mods-available/ldap.ini.erb'),
         require => Package['php5-ldap'],
         notify  => Exec['php::restart'],
     }

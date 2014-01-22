d379fe4bf8b1c0f4fe8fbc47c22a4d4d2b8b922d
Ben Chavet <ben.chavet@lullabot.com>
2014-01-22 17:43:40 -0600

    update php5-xmlrpc to trusty

--- modules/php/manifests/xmlrpc.pp
+++ modules/php/manifests/xmlrpc.pp
@@ -1,8 +1,8 @@
 class php::xmlrpc {
     if !defined(Package['php5-xmlrpc']) { package { 'php5-xmlrpc': require => Package['php5-cli'] } }
 
-    file { '/etc/php5/conf.d/xmlrpc.ini':
-        content => template('php/etc/php5/conf.d/xmlrpc.ini.erb'),
+    file { '/etc/php5/mods-available/xmlrpc.ini':
+        content => template('php/etc/php5/mods-available/xmlrpc.ini.erb'),
         require => Package['php5-xmlrpc'],
         notify  => Exec['php::restart'],
     }

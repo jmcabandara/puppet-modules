f2581f5779763343a08f887ffc78f053d26f5fab
Ben Chavet <ben.chavet@lullabot.com>
2014-01-22 17:42:09 -0600

    update php5-oauth to trusty

--- modules/php/manifests/oauth.pp
+++ modules/php/manifests/oauth.pp
@@ -1,17 +1,9 @@
 class php::oauth {
-    if !defined(Package['build-essential']) { package { 'build-essential': } }
-    if !defined(Package['libpcre3-dev']) { package { 'libpcre3-dev': } }
+    if !defined(Package['php5-oauth']) { package { 'php5-oauth': } }
 
-    exec { 'php::oauth::install':
-        command => 'pecl install oauth',
-        unless  => 'pecl list | grep oauth',
-        require => Package['php-pear', 'php5-dev', 'libpcre3-dev', 'build-essential'],
-        notify  => Exec['php::restart'],
-    }
-
-    file { '/etc/php5/conf.d/oauth.ini':
-        content => template('php/etc/php5/conf.d/oauth.ini.erb'),
-        require => Exec['php::oauth::install'],
+    file { '/etc/php5/mods-available/oauth.ini':
+        content => template('php/etc/php5/mods-available/oauth.ini.erb'),
+        require => Package['php5-oauth'],
         notify  => Exec['php::restart'],
     }
 }

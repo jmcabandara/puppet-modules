a4fbf2c7effaa707de5687959d7d3585667288e1
Ben Chavet <ben.chavet@lullabot.com>
2014-02-27 15:11:35 -0600

    Make sure a gem is only installed once

--- modules/ruby/manifests/gem.pp
+++ modules/ruby/manifests/gem.pp
@@ -2,9 +2,11 @@ define ruby::gem ($gem = $title) {
 
     if !defined(Package['rubygems']) { package { 'rubygems': } }
 
-    package { "ruby::gem::$gem":
-        name     => $gem,
-        provider => gem,
-        require  => Package['rubygems'],
+    if !defined(Package["ruby::gem::${gem}"]) {
+        package { "ruby::gem::${gem}":
+            name     => $gem,
+            provider => gem,
+            require  => Package['rubygems'],
+        }
     }
 }

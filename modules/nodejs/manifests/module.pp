0d887b1f8578409b5fdc75345bd9923593ed493e
Ben Chavet <ben.chavet@lullabot.com>
2014-02-27 15:10:50 -0600

    Make sure an npm module is only installed once

--- modules/nodejs/manifests/module.pp
+++ modules/nodejs/manifests/module.pp
@@ -1,10 +1,12 @@
 define nodejs::module ($module = $title) {
     if !defined(Package['npm']) { package { 'npm': } }
 
-    exec { "node.js::module::${module}":
-        command => "npm install -g ${module}",
-        require => Package['npm'],
-        creates => "/usr/lib/node_modules/${module}",
+    if !defined(Exec["nodejs::module::${module}"]) {
+        exec { "nodejs::module::${module}":
+            command => "npm install -g ${module}",
+            require => Package['npm'],
+            creates => "/usr/lib/node_modules/${module}",
+        }
     }
 
 }

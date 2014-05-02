25089c9f8b7e42e3bf54f0b021315a03a8558856
Ben Chavet <ben.chavet@lullabot.com>
2014-05-02 15:30:54 -0500

    Validate openvpn. Move jenkins-ci.org repo to jenkins module since ubuntu removed it from their repo

--- modules/jenkins/manifests/init.pp
+++ modules/jenkins/manifests/init.pp
@@ -1,5 +1,15 @@
 class jenkins {
 
+    apt::source { 'jenkins':
+        location    => 'http://pkg.jenkins-ci.org/debian',
+        release     => 'binary/',
+        repos       => '',
+        include_src => false,
+        key         => '10AF40FE',
+        key_source  => 'http://pkg.jenkins-ci.org/debian/jenkins-ci.org.key',
+        before      => Package['jenkins'],
+    }
+
     if !defined(Package['jenkins']) { package { 'jenkins': } }
 
     service { 'jenkins':

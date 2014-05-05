2949f024cd7c118f8452cb2b5ae3f0e41fd27c4b
Ben Chavet <ben.chavet@lullabot.com>
2014-05-05 08:31:02 -0500

    Keep authz_groupfile from re-running

--- modules/apache/manifests/mod/authz_groupfile.pp
+++ modules/apache/manifests/mod/authz_groupfile.pp
@@ -1,7 +1,7 @@
 class apache::mod::authz_groupfile {
 
     exec { 'a2enmod authz_groupfile':
-        creates => '/etc/apache2/mods-enabled/authn_groupfile.load',
+        creates => '/etc/apache2/mods-enabled/authz_groupfile.load',
         require => Package['apache2'],
         notify  => Service['apache2'],
     }

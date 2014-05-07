51aa6f26b1f59a4b92917a564cbb0249b0c91c39
Ben Chavet <ben.chavet@lullabot.com>
2014-05-07 16:44:47 -0500

    add nodejs-legacy package to make sure /usr/bin/node points to node.js

--- modules/nodejs/manifests/init.pp
+++ modules/nodejs/manifests/init.pp
@@ -1,6 +1,7 @@
 class nodejs {
 
     if !defined(Package['nodejs']) { package { 'nodejs': } }
+    if !defined(Package['nodejs-legacy']) { package { 'nodejs-legacy': } }
     if !defined(Package['npm']) { package { 'npm': } }
 
 }

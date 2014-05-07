2ebfa483bc620299aa70562a276262589b3ee3d6
Ben Chavet <ben.chavet@lullabot.com>
2014-05-07 16:35:06 -0500

    require both ruby and ruby-gem for ruby::gem

--- modules/ruby/manifests/gem.pp
+++ modules/ruby/manifests/gem.pp
@@ -1,12 +1,13 @@
 define ruby::gem ($gem = $title) {
 
+    if !defined(Package['ruby']) { package { 'ruby': } }
     if !defined(Package['ruby-dev']) { package { 'ruby-dev': } }
 
     if !defined(Package["ruby::gem::${gem}"]) {
         package { "ruby::gem::${gem}":
             name     => $gem,
             provider => gem,
-            require  => Package['ruby-dev'],
+            require  => Package['ruby', 'ruby-dev'],
         }
     }
 }

dfb6481c03bcd7880533835ad2febc6189bcb6f2
Ben Chavet <ben.chavet@lullabot.com>
2014-05-07 16:33:34 -0500

    ruby-dev needs to be installed in order for ruby gem to work

--- modules/ruby/manifests/gem.pp
+++ modules/ruby/manifests/gem.pp
@@ -1,12 +1,12 @@
 define ruby::gem ($gem = $title) {
 
-    if !defined(Package['ruby']) { package { 'ruby': } }
+    if !defined(Package['ruby-dev']) { package { 'ruby-dev': } }
 
     if !defined(Package["ruby::gem::${gem}"]) {
         package { "ruby::gem::${gem}":
             name     => $gem,
             provider => gem,
-            require  => Package['ruby'],
+            require  => Package['ruby-dev'],
         }
     }
 }

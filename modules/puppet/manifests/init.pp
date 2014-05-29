b28a428fa8ddc1e2e97f8cd70a01021235ab875d
Ben Chavet <ben.chavet@lullabot.com>
2014-05-29 15:00:39 -0500

    Puppet is already installed by the time this is run, or it would not be running

--- modules/puppet/manifests/init.pp
+++ modules/puppet/manifests/init.pp
@@ -1,30 +1,5 @@
 class puppet {
 
-    File {
-        require => Package['puppet'],
-        owner   => 'root',
-        group   => 'root',
-        mode    => '0644',
-    }
-
-    apt::source { 'puppetlabs':
-        location   => 'http://apt.puppetlabs.com',
-        repos      => 'main dependencies',
-        key        => '4BD6EC30',
-        key_server => 'pgp.mit.edu',
-        before     => Package['puppet'],
-    }
-
-    if !defined(Package['puppet']) { package { 'puppet': } }
-
-    file { '/etc/puppet/puppet.conf':
-        source => 'puppet:///modules/puppet/etc/puppet/puppet.conf',
-    }
-    
-    file { '/etc/puppet/hiera.yaml':
-        source => 'puppet:///modules/puppet/etc/puppet/hiera.yaml',
-    }
-
     cron { 'puppet':
         ensure  => present,
         command => '/usr/bin/puppet agent --onetime --no-daemonize --logdest syslog > /dev/null 2>&1',
@@ -35,7 +10,6 @@ class puppet {
     service { 'puppet':
         ensure  => stopped,
         enable  => false,
-        require => Package['puppet'],
     }
 
 }

b6b3837799999e79f203716883e33afea03a0d06
Ben Chavet <ben.chavet@lullabot.com>
2014-06-23 15:07:18 -0500

    Include ruby-dev and rbenv to ruby installation

--- modules/ruby/manifests/init.pp
+++ modules/ruby/manifests/init.pp
@@ -1,3 +1,5 @@
 class ruby {
     if !defined(Package['ruby']) { package { 'ruby': } }
+    if !defined(Package['ruby-dev']) { package { 'ruby-dev': } }
+    if !defined(Package['rbenv']) { package { 'rbenv': } }
 }

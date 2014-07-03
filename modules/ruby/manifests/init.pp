ef7cd1653cc6c3d6fee72ce133e4305778225f19
Ben Chavet <ben.chavet@lullabot.com>
2014-07-03 14:36:53 -0500

    Install ruby-build so rbenv can actually install different ruby versions

--- modules/ruby/manifests/init.pp
+++ modules/ruby/manifests/init.pp
@@ -2,4 +2,5 @@ class ruby {
     if !defined(Package['ruby']) { package { 'ruby': } }
     if !defined(Package['ruby-dev']) { package { 'ruby-dev': } }
     if !defined(Package['rbenv']) { package { 'rbenv': } }
+    if !defined(Package['ruby-build']) { package { 'ruby-build': } }
 }

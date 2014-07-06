a5cfe1bf542b7d6817a0cdd8249de85d03d00ca3
Ben Chavet <ben.chavet@lullabot.com>
2014-07-06 14:11:04 -0500

    Initialize rbenv globally if it is installed. #63

--- modules/ruby/manifests/init.pp
+++ modules/ruby/manifests/init.pp
@@ -3,4 +3,8 @@ class ruby {
     if !defined(Package['ruby-dev']) { package { 'ruby-dev': } }
     if !defined(Package['rbenv']) { package { 'rbenv': } }
     if !defined(Package['ruby-build']) { package { 'ruby-build': } }
+
+    file { '/etc/profile.d/rbenv.sh':
+        content => template('ruby/etc/profile.d/rbenv.sh.erb'),
+    }
 }

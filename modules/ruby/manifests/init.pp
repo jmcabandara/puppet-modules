3a43310d0adea07fc5f82f004d444f9b4eb63b8a
Ben Chavet <ben.chavet@lullabot.com>
2014-07-11 16:51:15 -0500

    Do not install ruby-build or rbenv with ruby

--- modules/ruby/manifests/init.pp
+++ modules/ruby/manifests/init.pp
@@ -1,10 +1,4 @@
 class ruby {
     if !defined(Package['ruby']) { package { 'ruby': } }
     if !defined(Package['ruby-dev']) { package { 'ruby-dev': } }
-    if !defined(Package['rbenv']) { package { 'rbenv': } }
-    if !defined(Package['ruby-build']) { package { 'ruby-build': } }
-
-    file { '/etc/profile.d/rbenv.sh':
-        content => template('ruby/etc/profile.d/rbenv.sh.erb'),
-    }
 }

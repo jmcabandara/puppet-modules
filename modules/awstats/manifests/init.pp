21af0dac441d5c2d3fb39f392e8948166d22ec24
Ben Chavet <ben.chavet@lullabot.com>
2014-07-23 12:41:11 -0500

    Manage awstats config directory, and keep it clean

--- modules/awstats/manifests/init.pp
+++ modules/awstats/manifests/init.pp
@@ -1,3 +1,10 @@
 class awstats {
     if !defined(Package['awstats']) { package { 'awstats': } }
+
+    file { '/etc/awstats':
+        ensure  => directory,
+        recurse => true,
+        purge   => true,
+        require => Package['awstats'],
+    }
 }

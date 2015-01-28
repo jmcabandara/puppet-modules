68921ce9d5c86c58c706aa1ad19f84da18e02f07
Ben Chavet <ben.chavet@lullabot.com>
2015-01-28 16:33:34 -0500

    Install couchdb on dev.lullabot.com. https://github.com/Lullabot/lullabot.com-edit/issues/11

---
+++ couchdb/manifests/init.pp
@@ -0,0 +1,31 @@
+class couchdb (
+    $httpd_port = 5984,
+    $httpd_bind_address = '127.0.0.1',
+    $httpd_enable_cors = false,
+    $cors_origins = undef,
+    $cors_methods = undef,
+    $cors_headers = undef,
+) {
+
+    if !defined(Package['couchdb']) {
+        package { 'couchdb': }
+    }
+
+    service { 'couchdb':
+        enable => true,
+        ensure => running,
+    }
+
+    file { '/etc/couchdb/local.d/httpd.ini':
+        content => template('couchdb/etc/couchdb/local.d/httpd.ini.erb'),
+        require => Package['couchdb'],
+        notify  => Service['couchdb'],
+    }
+
+    file { '/etc/couchdb/local.d/cors.ini':
+        content => template('couchdb/etc/couchdb/local.d/cors.ini.erb'),
+        require => Package['couchdb'],
+        notify  => Service['couchdb'],
+    }
+
+}

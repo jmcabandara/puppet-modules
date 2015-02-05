e62aef253840f62acaccaa8ab6ffde7c0939af9b
Ben Chavet <ben.chavet@lullabot.com>
2015-02-05 14:59:58 -0500

    Move puppet-managed config to default.d/

--- couchdb/manifests/init.pp
+++ couchdb/manifests/init.pp
@@ -16,20 +16,16 @@ class couchdb (
         ensure => running,
     }
 
-    file { '/etc/couchdb/local.d/httpd.ini':
-        content => template('couchdb/etc/couchdb/local.d/httpd.ini.erb'),
+    file { '/etc/couchdb/default.d/httpd.ini':
+        content => template('couchdb/etc/couchdb/default.d/httpd.ini.erb'),
         require => Package['couchdb'],
         notify  => Service['couchdb'],
-        owner   => 'couchdb',
-        group   => 'couchdb',
     }
 
-    file { '/etc/couchdb/local.d/cors.ini':
-        content => template('couchdb/etc/couchdb/local.d/cors.ini.erb'),
+
+    file { '/etc/couchdb/default.d/cors.ini':
+        content => template('couchdb/etc/couchdb/default.d/cors.ini.erb'),
         require => Package['couchdb'],
         notify  => Service['couchdb'],
-        owner   => 'couchdb',
-        group   => 'couchdb',
     }
-
 }

29b2d3bb4cc094d6be880b7bafa935c6feb4a4c4
Ben Chavet <ben.chavet@lullabot.com>
2015-03-09 14:53:57 -0400

    add docker defaults file to puppet management

--- docker/manifests/init.pp
+++ docker/manifests/init.pp
@@ -1,5 +1,6 @@
 class docker (
     $package = 'docker.io',
+    $docker_opts = undef,
 ) {
 
     apt::source { 'docker':
@@ -16,7 +17,22 @@ class docker (
         name => $package,
     }
 
+    file { '/etc/default/docker':
+        path    => $package ? {
+            'docker.io'  => '/etc/default/docker.io',
+            'lxc-docker' => '/etc/default/docker',
+            default      => '/etc/default/docker',
+        },
+        content => template('docker/etc/default/docker.erb'),
+        require => Package['docker'],
+        notify  => Service['docker']
+    }
+
     service { 'docker':
+        name    => $package ? {
+            'lxc-docker' => 'docker',
+            default      => $package,
+        },
         ensure  => running,
         enable  => true,
         require => Package['docker'],

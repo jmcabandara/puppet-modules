a71a8539133c4ef45eb7bd4d400964fd450f40c5
Ben Chavet <ben.chavet@lullabot.com>
2015-04-30 11:39:47 -0400

    Use the nodesource repo for node.js insteady of Ubuntu universe

--- nodejs/manifests/init.pp
+++ nodejs/manifests/init.pp
@@ -1,7 +1,15 @@
-class nodejs {
+class nodejs (
+    $version = '0.10',
+) {
+
+    apt::source { 'nodesource':
+        location   => "https://deb.nodesource.com/node_${version}",
+        repos      => 'main',
+        key        => 'AA01DA2C',
+        key_source => 'https://deb.nodesource.com/gpgkey/nodesource.gpg.key',
+        before     => Package['nodejs'],
+    }
 
     if !defined(Package['nodejs']) { package { 'nodejs': } }
-    if !defined(Package['nodejs-legacy']) { package { 'nodejs-legacy': } }
-    if !defined(Package['npm']) { package { 'npm': } }
 
 }
--- nodejs/manifests/module.pp
+++ nodejs/manifests/module.pp
@@ -1,10 +1,10 @@
 define nodejs::module ($module = $title) {
-    if !defined(Package['npm']) { package { 'npm': } }
+
+    require ::nodejs
 
     if !defined(Exec["nodejs::module::${module}"]) {
         exec { "nodejs::module::${module}":
             command => "npm install -g ${module}",
-            require => Package['npm'],
             creates => "/usr/local/lib/node_modules/${module}",
         }
     }

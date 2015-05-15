b182beadd0bb4fc53f3943a8a6907fc43fa91bbe
Ben Chavet <ben.chavet@lullabot.com>
2015-05-15 16:20:31 -0400

    Add a domain_name parameter to the avahi class

--- avahi/manifests/init.pp
+++ avahi/manifests/init.pp
@@ -1,6 +1,16 @@
-class avahi {
+class avahi (
+    $domain_name = 'local',
+) {
 
-    if !defined(Package['avahi-daemon']) { package { 'avahi-daemon': } }
+    if !defined(Package['avahi-daemon']) {
+        package { 'avahi-daemon': }
+    }
+
+    file { '/etc/avahi/avahi-daemon.conf':
+        content => template('avahi/etc/avahi/avahi-daemon.conf.erb'),
+        require => Package['avahi-daemon'],
+        notify  => Service['avahi-daemon'],
+    }
 
     service { 'avahi-daemon':
         ensure  => running,

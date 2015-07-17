f40dee8bd91a750b55b044cccf34ded2fe1eaf7c
Ben Chavet <ben.chavet@lullabot.com>
2015-07-17 16:19:08 -0400

    lxc-docker is installed by default

--- docker/manifests/init.pp
+++ docker/manifests/init.pp
@@ -47,72 +47,70 @@ class docker (
     }
 
     # Recompile lxc-docker from source to enable udev sync
-    if ($package == 'lxc-docker') {
-        require ::btrfs
+    require ::btrfs
 
-        if !defined(Package['build-essential']) {
-            package { 'build-essential': }
-        }
+    if !defined(Package['build-essential']) {
+        package { 'build-essential': }
+    }
 
-        if !defined(Package['golang']) {
-            package { 'golang': }
-        }
+    if !defined(Package['golang']) {
+        package { 'golang': }
+    }
 
-        if !defined(Package['golang-gosqlite-dev']) {
-            package { 'golang-gosqlite-dev': }
-        }
+    if !defined(Package['golang-gosqlite-dev']) {
+        package { 'golang-gosqlite-dev': }
+    }
 
-        if !defined(Package['libdevmapper-dev']) {
-            package { 'libdevmapper-dev': }
-        }
+    if !defined(Package['libdevmapper-dev']) {
+        package { 'libdevmapper-dev': }
+    }
 
-        exec { 'docker::download':
-            command => 'git clone https://git@github.com/docker/docker',
-            cwd     => '/usr/local/src',
-            creates => '/usr/local/src/docker',
-            require => Package['build-essential', 'golang', 'golang-gosqlite-dev', 'libdevmapper-dev', 'lxc-docker'],
-        }
+    exec { 'docker::download':
+        command => 'git clone https://git@github.com/docker/docker',
+        cwd     => '/usr/local/src',
+        creates => '/usr/local/src/docker',
+        require => Package['build-essential', 'golang', 'golang-gosqlite-dev', 'libdevmapper-dev', 'lxc-docker'],
+    }
 
-        exec { 'docker::fetch':
-            command => 'git fetch --all',
-            cwd     => '/usr/local/src/docker',
-            require => Exec['docker::download'],
-        }
+    exec { 'docker::fetch':
+        command => 'git fetch --all',
+        cwd     => '/usr/local/src/docker',
+        require => Exec['docker::download'],
+    }
 
-        exec { 'docker::version':
-            command  => "git checkout v${version}",
-            cwd      => '/usr/local/src/docker',
-            require  => Exec['docker::fetch'],
-            unless  => "git status | grep \"HEAD detached at v${version}\"",
-        }
+    exec { 'docker::version':
+        command  => "git checkout v${version}",
+        cwd      => '/usr/local/src/docker',
+        require  => Exec['docker::fetch'],
+        unless  => "git status | grep \"HEAD detached at v${version}\"",
+    }
 
-        exec { 'docker::compile':
-            command     => '/usr/local/src/docker/hack/make.sh dynbinary',
-            cwd         => '/usr/local/src/docker',
-            environment => ['AUTO_GOPATH=1'],
-            require     => Exec['docker::version'],
-            unless      => "test -d /usr/local/src/docker/bundles/${version}",
-        }
+    exec { 'docker::compile':
+        command     => '/usr/local/src/docker/hack/make.sh dynbinary',
+        cwd         => '/usr/local/src/docker',
+        environment => ['AUTO_GOPATH=1'],
+        require     => Exec['docker::version'],
+        unless      => "test -d /usr/local/src/docker/bundles/${version}",
+    }
 
-        exec { 'docker::install::docker':
-            command     => "install -m 755 -o root -g root /usr/local/src/docker/bundles/${version}/dynbinary/docker-${version} /usr/bin/docker",
-            require     => Exec['docker::compile'],
-            notify      => Service['docker'],
-            unless      => "/usr/bin/docker --version | grep \"Docker version ${version},\"",
-        }
+    exec { 'docker::install::docker':
+        command     => "install -m 755 -o root -g root /usr/local/src/docker/bundles/${version}/dynbinary/docker-${version} /usr/bin/docker",
+        require     => Exec['docker::compile'],
+        notify      => Service['docker'],
+        unless      => "/usr/bin/docker --version | grep \"Docker version ${version},\"",
+    }
 
-        exec { 'docker::install::dockerinit-version':
-            command     => "install -m 755 -o root -g root /usr/local/src/docker/bundles/${version}/dynbinary/dockerinit-${version} /var/lib/docker/init/dockerinit-${version}",
-            subscribe   => Exec['docker::install::docker'],
-            notify      => Service['docker'],
-            refreshonly => true,
-        }
+    exec { 'docker::install::dockerinit-version':
+        command     => "install -m 755 -o root -g root /usr/local/src/docker/bundles/${version}/dynbinary/dockerinit-${version} /var/lib/docker/init/dockerinit-${version}",
+        subscribe   => Exec['docker::install::docker'],
+        notify      => Service['docker'],
+        refreshonly => true,
+    }
 
-        exec { 'docker::install::dockerinit':
-            command     => "install -m 755 -o root -g root /usr/local/src/docker/bundles/${version}/dynbinary/dockerinit-${version} /usr/bin/dockerinit",
-            subscribe   => Exec['docker::install::docker'],
-            notify      => Service['docker'],
-            refreshonly => true,
-        }
+    exec { 'docker::install::dockerinit':
+        command     => "install -m 755 -o root -g root /usr/local/src/docker/bundles/${version}/dynbinary/dockerinit-${version} /usr/bin/dockerinit",
+        subscribe   => Exec['docker::install::docker'],
+        notify      => Service['docker'],
+        refreshonly => true,
     }
 }

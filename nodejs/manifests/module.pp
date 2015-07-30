d32e724d52e73493572d4757521caad77e822949
Ben Chavet <ben.chavet@lullabot.com>
2015-07-30 14:45:41 -0400

    Provide an option $version parameter to nodejs::module

--- nodejs/manifests/module.pp
+++ nodejs/manifests/module.pp
@@ -1,11 +1,20 @@
-define nodejs::module ($module = $title) {
+define nodejs::module (
+    $module = $title,
+    $version = undef,
+) {
 
     require ::nodejs
 
     if !defined(Exec["nodejs::module::${module}"]) {
         exec { "nodejs::module::${module}":
-            command => "npm install -g ${module}",
-            creates => "/usr/local/lib/node_modules/${module}",
+            command => $version ? {
+                undef   => "npm install -g ${module}",
+                default => "npm install -g ${module}@${version}",
+            },
+            unless  => $version ? {
+                undef   => "npm -g ls ${module}",
+                default => "npm -g ls ${module}@${version}",
+            },
         }
     }
 

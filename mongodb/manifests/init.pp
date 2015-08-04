2eb77cbda8adf032490273befc32283133257c4b
Ben Chavet <ben.chavet@lullabot.com>
2015-08-03 22:24:26 -0400

    Install mongodb on the tugboat dashboard server

---
+++ mongodb/manifests/init.pp
@@ -0,0 +1,35 @@
+class mongodb (
+    $bind_ip = '127.0.0.1',
+    $syslog = false,
+) {
+
+    include apt
+
+    apt::source { 'mongodb':
+        location    => 'http://repo.mongodb.org/apt/ubuntu',
+        release     => 'trusty/mongodb-org/3.0',
+        repos       => 'multiverse',
+        key         => '7F0CEB10',
+        key_server  => 'keyserver.ubuntu.com',
+        include_src => false,
+        before      => Package['mongodb'],
+    }
+
+    if !defined(Package['mongodb']) {
+        package { 'mongodb':
+            name => 'mongodb-org',
+        }
+    }
+
+    file { '/etc/mongod.conf':
+        content => template('mongodb/etc/mongod.conf.erb'),
+        require => Package['mongodb'],
+        notify  => Service['mongod'],
+    }
+
+    service { 'mongod':
+        ensure  => running,
+        enable  => true,
+        require => Package['mongodb'],
+    }
+}

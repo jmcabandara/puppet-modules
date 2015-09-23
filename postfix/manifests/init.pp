c2418196db61c0fca97970a2cdc4ee02a3e78f68
Ben Chavet <ben.chavet@lullabot.com>
2015-09-23 17:24:16 -0400

    Add optional parameter to postfix class to configure mailbox_command

--- postfix/manifests/init.pp
+++ postfix/manifests/init.pp
@@ -4,6 +4,7 @@ class postfix (
     $mydestination = "$::fqdn, localhost",
     $mynetworks = '127.0.0.0/8 [::ffff:127.0.0.0]/104 [::1]/128',
     $canonical_maps = undef,
+    $mailbox_command = undef,
 ) {
 
     File {

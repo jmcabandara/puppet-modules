f3a79e5a22755bdcd680843b135e9bbb7fed0d88
Ben Chavet <ben.chavet@lullabot.com>
2016-01-29 13:59:10 -0500

    Merge branch 'dns-update'

--- bind/manifests/init.pp
+++ bind/manifests/init.pp
@@ -1,14 +1,18 @@
 class bind (
+    $directory = '/var/cache/bind',
     $forwarders = [],
-    $allow_new_zones = true,
-    $response_policy = undef,
+    $zone_notify = [],
+    $also_notify = [],
 ) {
 
-    if !defined(Package['bind9']) { package { 'bind9': } }
+    if !defined(Package['bind9']) {
+        package { 'bind9': }
+    }
 
-    service { 'bind9':
-        ensure => running,
-        enable => true,
+    file { '/etc/bind':
+        ensure => directory,
+        require => Package['bind9'],
+        mode    => '0775',
     }
 
     file { '/etc/bind/named.conf.options':
@@ -17,10 +21,15 @@ class bind (
         notify  => Service['bind9'],
     }
 
-    # Get a fresh copy of the active zones
-    exec { 'bind::dump.db':
-        command => "rndc dumpdb -zones",
-        require => Service['bind9'],
+    file { '/etc/bind/named.conf.local':
+        ensure  => present,
+        require => Package['bind9'],
+        mode    => '0664',
+    }
+
+    service { 'bind9':
+        ensure => running,
+        enable => true,
     }
 
 }

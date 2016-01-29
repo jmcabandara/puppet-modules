8ecca1edc7d9a5eabbeb80cd5597c2124278acbb
Ben Chavet <ben.chavet@lullabot.com>
2016-01-29 14:04:18 -0500

    Install bind on ops.lullabot.com

--- bind/manifests/init.pp
+++ bind/manifests/init.pp
@@ -9,22 +9,10 @@ class bind (
         package { 'bind9': }
     }
 
-    file { '/etc/bind':
-        ensure => directory,
-        require => Package['bind9'],
-        mode    => '0775',
-    }
-
     file { '/etc/bind/named.conf.options':
         content => template('bind/etc/bind/named.conf.options.erb'),
         require => Package['bind9'],
         notify  => Service['bind9'],
-    }
-
-    file { '/etc/bind/named.conf.local':
-        ensure  => present,
-        require => Package['bind9'],
-        mode    => '0664',
     }
 
     service { 'bind9':

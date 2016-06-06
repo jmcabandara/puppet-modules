113e7b2fa40ea0a87d264a27bfdbc554113f0339
Ben Chavet <ben.chavet@lullabot.com>
2016-06-06 16:15:24 -0400

    Install strongswan-plugin-unity

--- strongswan/manifests/init.pp
+++ strongswan/manifests/init.pp
@@ -4,4 +4,9 @@ class strongswan {
         package { 'strongswan': }
     }
 
+    if !defined(Package['strongswan-plugin-unity']) {
+        package { 'strongswan-plugin-unity':
+            require => Package['strongswan'],
+        }
+    }
 }

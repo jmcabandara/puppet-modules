43cf9446375edd8fb0753b103f40209a75e44362
Ben Chavet <ben.chavet@lullabot.com>
2016-09-21 18:18:51 -0400

    disable resolvconf override

--- unbound/manifests/init.pp
+++ unbound/manifests/init.pp
@@ -1,7 +1,15 @@
-class unbound {
+class unbound (
+    $resolvconf = true,
+) {
 
     if !defined(Package['unbound']) {
         package { 'unbound': }
+    }
+
+    file { '/etc/default/unbound':
+        content => template('unbound/etc/default/unbound.erb'),
+        require => Package['unbound'],
+        notify  => Service['unbound'],
     }
 
     service { 'unbound':
